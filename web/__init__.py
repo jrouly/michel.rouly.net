d80bf02d2deb669550c6750d524c9420aac901ba
Jean Michel Rouly <michel.rouly@gmail.com>
2015-03-29 17:15:40 -0400

    linked error views to right template

--- web/__init__.py
+++ web/__init__.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template('error.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('error.html'), 500
 
 from web import views
 app.register_blueprint(views.mod)

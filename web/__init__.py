6b8c5241aff11e644686bc46d80cdcac51f60148
Michel Rouly <michel@rouly.net>
2025-05-11 12:26:07 -0700

    more aggressive caching

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,24 @@
+from datetime import timedelta
 from flask import Flask, render_template
 from flask_minify import Minify
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from web import views
+from web.cache import cache
 
+config = {
+    "SEND_FILE_MAX_AGE_DEFAULT": timedelta(days=30),
+}
+
+# Initialize application.
 app = Flask(__name__)
+app.config.from_mapping(config)
+
+# Configure application.
 Minify(app=app)
+cache.init_app(app)
+app.register_blueprint(views.mod)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -14,7 +27,3 @@ def not_found(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('error.html'), 500
-
-app.register_blueprint(views.mod)
-
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
--- web/views.py
+++ web/views.py
@@ -1,24 +1,30 @@
 from flask import Blueprint, render_template, url_for, redirect
+from web.cache import cache
 
 mod = Blueprint('views', __name__)
 
 @mod.route('/')
+@cache.cached()
 def homepage():
     return render_template("index.html")
 
 @mod.route('/work')
+@cache.cached()
 def work():
     return render_template("work.html")
 
 @mod.route('/projects')
+@cache.cached()
 def projects():
     return render_template("projects.html")
 
 @mod.route('/research')
+@cache.cached()
 def research():
     return render_template("research.html")
 
 @mod.route('/resume')
 @mod.route('/resume.pdf')
+@cache.cached()
 def resume():
     return redirect(url_for('static', filename='rouly_resume.pdf'))
